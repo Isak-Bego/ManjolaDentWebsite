7b708f5c1826bdaba48c481263faf401c564c8fe
Isak-Bego <isakbego@gmail.com>
2022-12-18 14:33:44 +0200

    Added the Whatsapp button:

--- code.js
+++ code.js
@@ -7,21 +7,20 @@ function myFunction(x) {
   } else menu[0].style.display = "none";
 }
 
-function scrolling(s){
-
-  const container = document.getElementsByClassName('container');
-  let prevSection; 
-  let height; 
-  if(s === 'klinika') window.scrollTo(0, 0); 
-  else if(s === 'sherbime'){
-    prevSection = document.getElementsByClassName('about-section'); 
-    height = prevSection[0].clientHeight; 
-    window.scrollTo(0, height); 
-  }else{
-    prevSection = document.getElementsByTagName('body'); 
-    height = prevSection[0].clientHeight; 
-    window.scrollTo(0,height)
+function scrolling(s) {
+  const container = document.getElementsByClassName("container");
+  let prevSection;
+  let height;
+  if (s === "klinika") window.scrollTo(0, 0);
+  else if (s === "sherbime") {
+    prevSection = document.getElementsByClassName("about-section");
+    height = prevSection[0].clientHeight;
+    window.scrollTo(0, height);
+  } else {
+    prevSection = document.getElementsByTagName("body");
+    height = prevSection[0].clientHeight;
+    window.scrollTo(0, height);
   }
-
-  myFunction(container[0]); 
+  
+  myFunction(container[0]);
 }
