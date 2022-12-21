cb1be0be6049f549ef5fa9043b051f336ea2702e
Isak-Bego <isakbego@gmail.com>
2022-12-21 15:02:58 +0100

    added the slideshow

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ function scrolling(s, navbar) {
     prevSection = document.getElementsByClassName("about-section");
     height = prevSection[0].clientHeight;
     window.scrollTo(0, height);
+  } else if (s === "paciente") {
+    prevSection = document.getElementsByClassName("about-section");
+    height = prevSection[0].clientHeight;
+    prevSection = document.getElementsByClassName("Sherbime");
+    height += prevSection[0].clientHeight + 30;
+    window.scrollTo(0, height);
   } else {
     prevSection = document.getElementsByTagName("body");
     height = prevSection[0].clientHeight;
@@ -48,4 +54,37 @@ function disableHiddenMenu() {
   if (window.innerWidth > 640) menu[0].style.display = "none";
   else if (container[0].className != "container")
     menu[0].style.display = "block";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
